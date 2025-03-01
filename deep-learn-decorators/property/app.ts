e35b4269a51b8530c5f9431be9bbb97bf49f7435
soheilMansoori <ahadmansoori369@gmail.com>
2025-03-01 13:55:02 +0330

    fix property Decorator internal validate

--- deep-learn-decorators/property/app.ts
+++ deep-learn-decorators/property/app.ts
@@ -36,7 +36,45 @@ function MinAge(limit: number) {
   };
 }
 
-// validate
+// MinLength Decorator
+function MaxLength(limit: number) {
+  return (target: any, propertyKey: string) => {
+    const metadataKey = `__metadata__maxLength__${propertyKey}`;
+    target.constructor[metadataKey] = {
+      key: propertyKey,
+      limit,
+      message: null,
+      checkValue: function (value: string): boolean {
+        if (value.length > limit) {
+          this.message = `${propertyKey} must be maximum length ${limit}`;
+          return false;
+        }
+        return true;
+      },
+    };
+  };
+}
+
+// MaxLength Decorator
+function MinLength(limit: number) {
+  return (target: any, propertyKey: string) => {
+    const metadataKey = `__metadata__minLength__${propertyKey}`;
+    target.constructor[metadataKey] = {
+      key: propertyKey,
+      limit,
+      message: null,
+      checkValue: function (value: string): boolean {
+        if (value.length < limit) {
+          this.message = `${propertyKey} must be minimum length ${limit}`;
+          return false;
+        }
+        return true;
+      },
+    };
+  };
+}
+
+// external validate
 const AgeValidator = (instance: any) => {
   const constructor = instance.constructor;
   const metadataKeys = Object.keys(constructor);
@@ -59,11 +97,35 @@ class User {
   @MaxAge(100)
   @MinAge(18)
   private age: number;
+
+  @MaxLength(12)
+  @MinLength(4)
   private name: string;
 
   constructor(name: string, age: number) {
     this.name = name;
     this.age = age;
+
+    // run internal validate
+    this.validate();
+  }
+
+  // internal validate
+  private validate(): void {
+    const constructor = this.constructor as any;
+    const metadataKeys = Object.keys(constructor);
+
+    for (const metadataKey of metadataKeys) {
+      const mainMetadata = constructor[metadataKey];
+      const value = (this as any)[mainMetadata.key];
+      const validationResult = mainMetadata.checkValue(value);
+
+      // check error messages
+      if (!validationResult) {
+        const message = mainMetadata.message;
+        throw new Error(message);
+      }
+    }
   }
 
   // get info
@@ -74,8 +136,9 @@ class User {
 
 try {
   const soheilUser = new User("soheil", 21);
-  // run validation
-  AgeValidator(soheilUser);
+
+  // run external validation
+  // AgeValidator(soheilUser);
 
   console.log("soheilUser => ", soheilUser.info());
 } catch (error) {
